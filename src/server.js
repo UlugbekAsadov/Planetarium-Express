f199d1a81448569392f468a20133684e732fa0e8
Ulugbek <ulugbek4work@gmail.com>
2023-04-06 23:20:25 +0500

    initial commit

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, () => console.log("Server is running on port " + PORT));
