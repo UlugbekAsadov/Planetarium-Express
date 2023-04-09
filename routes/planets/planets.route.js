4e7caae56b9235d916da030a0d91cb3b4df324a3
Ulugbek <ulugbek4work@gmail.com>
2023-04-09 15:09:32 +0500

    Routed protected

--- routes/planets/planets.route.js
+++ routes/planets/planets.route.js
@@ -4,15 +4,16 @@ const {
   createNewPlanet,
   getPlanetById,
   updatePlanet,
-  deletePlanet
+  deletePlanet,
 } = require("../../controllers/planets/planet.controller");
 const upload = require("../../utils/file-upload");
 const router = Router();
+const { protected, adminAccess } = require("../../middlewares/auth");
 
 router.get("/", getAllPlanets);
 router.get("/:id", getPlanetById);
 router.post("/", upload.single("image"), createNewPlanet);
-router.put("/:id", updatePlanet);
-router.delete("/:id", deletePlanet);
+router.put("/:id", protected, updatePlanet);
+router.delete("/:id", protected, adminAccess, deletePlanet);
 
 module.exports = router;
